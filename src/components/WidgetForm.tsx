bcd8e2b8008917a1cd4807610e19cfc8ffc54777
Gean Gomes <geansg4@gmail.com>
2022-06-07 11:18:04 -0300

    chore: add feedback types

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -3,6 +3,7 @@ import { CloseButton } from "./CloseButton"
 import bugImageUrl from '../assets/bug.svg'
 import ideaImageUrl from '../assets/idea.svg'
 import thoughtImageUrl from '../assets/thought.svg'
+import { useState } from "react"
 
 const feedbacktypes = {
   BUG: {
@@ -28,20 +29,26 @@ const feedbacktypes = {
   },
 }
 
+type FeedbackTypeProps = keyof typeof feedbacktypes
+
 export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackTypeProps | null>(null)
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       <header>
         <span className="text-xl leading-6">Deixe seu feedback</span>
         <CloseButton />
       </header>
-      <div className="flex py-8 gap-2 w-full">
+      {!feedbackType ? (
+        <div className="flex py-8 gap-2 w-full">
         {Object.entries(feedbacktypes).map(([key, value]) => {
           return (
             <button
               key={key}
               className="bg-zinc-800 py-5 rounded-lg w-24 flex flex-1 flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
               type="button"
+              onClick={() => setFeedbackType(key as FeedbackTypeProps)}
             >
               <img src={value.image.source} alt={value.image.alt} />
               <span>{value.title}</span>
@@ -49,7 +56,9 @@ export function WidgetForm() {
           )
         })}
       </div>
-
+      ) : (
+        <p>Hello World</p>
+      )}
       <footer>
         <span className="text-xs text-neutral-400">Feito com ♥</span>
       </footer>
